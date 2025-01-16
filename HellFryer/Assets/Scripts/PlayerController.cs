4f8bf788c7828b0995096f9c22398f6b9a261527
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-01-16 10:42:56 +0100

    Implemented controller support for player input

--- HellFryer/Assets/Scripts/PlayerController.cs
+++ HellFryer/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerController : MonoBehaviour
 {
+    PlayerMovement mover;
+
     void Start()
     {
-        
+        mover = GetComponent<PlayerMovement>();
     }
 
-    void Update()
+    public void OnMove(CallbackContext context)
     {
-        
+        mover.SetInputVector(context.ReadValue<Vector2>());
     }
 }
--- HellFryer/Assets/Scripts/PlayerMovement.cs
+++ HellFryer/Assets/Scripts/PlayerMovement.cs
@@ -7,17 +7,21 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float speed = 100;
 
+    private Vector2 inputVector = Vector2.zero;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    public void SetInputVector(Vector2 direction)
+    {
+        inputVector = direction;
+    }
+
     void Update()
     {
-        float verticalMovement = Input.GetAxis("Vertical");
-        float horizontalMovement = Input.GetAxis("Horizontal");
-
-        Vector3 velocity = Vector3.Normalize(new Vector3(horizontalMovement, 0, verticalMovement));
+        Vector3 velocity = Vector3.Normalize(new Vector3(inputVector.x, 0, inputVector.y));
         velocity *= Time.deltaTime * speed;
 
         rb.velocity = velocity;
