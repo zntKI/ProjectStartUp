a69883ac03aec320b2decc78a98effeb23d19d61
Nikola <nikolagoev9@gmail.com>
2025-01-21 13:48:48 +0100

    Added custom prefab settings for each player

--- HellFryer/Assets/Scripts/InputManager.cs
+++ HellFryer/Assets/Scripts/InputManager.cs
@@ -5,13 +5,27 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    public GameObject playerPrefab;
+    public Transform player2SelectorContent;
     private PlayerInputManager inputManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        inputManager = GetComponent<PlayerInputManager>();
-        inputManager.JoinPlayer();
+        if (playerPrefab != null && player2SelectorContent !=  null) {
+            InventorySelector player2Selector = playerPrefab.GetComponentInChildren<InventorySelector>();
+            player2Selector.SelectorContent = player2SelectorContent;
+
+            inputManager = GetComponent<PlayerInputManager>();
+            inputManager.playerPrefab = playerPrefab;
+            inputManager.JoinPlayer();
+        }
+        else
+        {
+            Debug.Log("Missing player 2 information");
+        }
+
+        
     }
 
     // Update is called once per frame
