7484576a1262e5b87cb4c8cb455110d5a6995ac6
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-01-24 13:47:42 +0100

    Implemented graveyard item dig out and zombie hold

---
+++ HellFryer/Assets/Scripts/Graveyard/DigAreaController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigAreaController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject limbsPrefab;
+    [SerializeField]
+    private GameObject heartPrefab;
+    [SerializeField]
+    private GameObject zomnbiePrefab;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.TryGetComponent<PlayerController>(out PlayerController player))
+        {
+            // Spawn item
+            DigOutItem gameObjectSpawned = (DigOutItem)Random.Range((int)DigOutItem.Limbs, (int)DigOutItem.Zombie + 1);
+            if (gameObjectSpawned == DigOutItem.Zombie)
+            {
+                // Spawn zombie
+                GameObject zombie = Instantiate(zomnbiePrefab, player.transform.position, Quaternion.identity);
+                Vector3 diffVec = (player.transform.position - transform.position).normalized;
+                Vector3 lookVec = new Vector3(diffVec.x, 0, diffVec.z);
+                zombie.transform.LookAt(player.transform.position + lookVec);
+
+                player.DisableMovement();
+            }
+            else
+            {
+                SpawnItem(gameObjectSpawned);
+            }
+
+            // Disable detection functionality
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    private void SpawnItem(DigOutItem prefabId)
+    {
+        Vector3 spawnPos = transform.position;
+        GameObject spawnedItem = null;
+
+        switch (prefabId)
+        {
+            case DigOutItem.Limbs:
+                spawnedItem = Instantiate(limbsPrefab, spawnPos, Quaternion.identity);
+                break;
+            case DigOutItem.Heart:
+                spawnedItem = Instantiate(heartPrefab, spawnPos, Quaternion.identity);
+                break;
+            default:
+                Debug.LogWarning("Incorrect spawn item Id in graveyard dig out");
+                return;
+        }
+    }
+
+}
+
+public enum DigOutItem
+{
+    Limbs = 0,
+    Heart,
+    Zombie,
+}
--- HellFryer/Assets/Scripts/Roles/Equipment/Gloves/GlovesHuntEquipmentStrategy.cs
+++ HellFryer/Assets/Scripts/Roles/Equipment/Gloves/GlovesHuntEquipmentStrategy.cs
@@ -5,13 +5,23 @@ using UnityEngine;
 
 public class GlovesHuntEquipmentStrategy : EquipmentStrategy
 {
+    [SerializeField]
+    private float digDetectRange = 1f;
+
     public override void StartUp()
     {
-        throw new System.NotImplementedException();
+        var hitColliders = Physics.OverlapSphere(transform.position, digDetectRange);
+        foreach (Collider hitCollider in hitColliders)
+        {
+            if (hitCollider.CompareTag("ZombieArm"))
+            {
+                Destroy(hitCollider.transform.parent.gameObject);
+                transform.parent.GetComponent<PlayerController>().EnableMovement();
+            }
+        }
     }
 
     public override void Perform()
     {
-        throw new System.NotImplementedException();
     }
 }
