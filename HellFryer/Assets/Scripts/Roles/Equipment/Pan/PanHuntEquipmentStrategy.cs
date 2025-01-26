1d680dc845f0123e1179fb9d3a7eabb0809aadfb
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-01-26 14:08:54 +0100

    Created pan prefab

---
+++ HellFryer/Assets/Scripts/Roles/Equipment/Pan/PanHuntEquipmentStrategy.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanHuntEquipmentStrategy : EquipmentStrategy
+{
+    public override void StartUp()
+    {
+    }
+
+    public override void Perform()
+    {
+    }
+}
