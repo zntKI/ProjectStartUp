e1aed9cacde40a07e79db9fcf2e89456a77f0e15
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-01-27 10:27:15 +0100

    Automated waypoint following
    - Made so that it collects its waypoints automatically instead of specifying them in the inspector

--- HellFryer/Assets/Scripts/DarkArea/WaypointFollower.cs
+++ HellFryer/Assets/Scripts/DarkArea/WaypointFollower.cs
@@ -7,20 +7,34 @@ public class WaypointFollower : MonoBehaviour
 {
     public event Action<Transform> OnWaypointChange;
 
-    [SerializeField]
-    private GameObject[] waypoints;
+    private List<Transform> waypoints;
 
     [SerializeField]
     private float speed = 2f;
 
     private int currentWaypointIndex = 0;
 
+    void Start()
+    {
+        waypoints = new List<Transform>();
+
+        Transform wingCreatureParent = transform.parent;
+        for (int i = 0; i < wingCreatureParent.childCount; i++)
+        {
+            Transform child = wingCreatureParent.GetChild(i);
+            if (child.CompareTag("WingCreatureWaypoint"))
+            {
+                waypoints.Add(child);
+            }
+        }
+    }
+
     void Update()
     {
         if (Vector3.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < .1f)
         {
             currentWaypointIndex++;
-            if (currentWaypointIndex >= waypoints.Length)
+            if (currentWaypointIndex >= waypoints.Count)
             {
                 currentWaypointIndex = 0;
             }
