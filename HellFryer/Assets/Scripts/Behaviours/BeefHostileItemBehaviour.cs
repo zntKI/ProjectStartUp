1cc357aa5a8b1f80c16a6ecd8760bf26fa92827b
Kaloyan Ivanov <kaloyanivanov@hotmail.com>
2025-01-27 21:44:31 +0100

    Implemented beef hostile behaviour

---
+++ HellFryer/Assets/Scripts/Behaviours/BeefHostileItemBehaviour.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeefHostileItemBehaviour : HostileItemBehaviour
+{
+    [SerializeField, Tooltip("given time to wait before starting to roam")]
+    private float timeToWait;
+    [SerializeField]
+    private float waypointFollowSpeed = 2f;
+
+    private BeefHostileState state;
+
+    private float timeCounter = 0f;
+
+    private GameObject[] waypointsToFollow;
+    private int currentWaypointIndex = 0;
+
+    void Start()
+    {
+        waypointsToFollow = GameObject.FindGameObjectsWithTag("BeefWaypoint");
+
+        state = BeefHostileState.None;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case BeefHostileState.None:
+                break;
+            case BeefHostileState.WaitingStatic:
+
+                timeCounter += Time.deltaTime;
+                if (timeCounter > timeToWait)
+                {
+                    state = BeefHostileState.Roaming;
+                }
+
+                break;
+            case BeefHostileState.Roaming:
+
+                FollowWaypoints();
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void FollowWaypoints()
+    {
+        if (Vector3.Distance(waypointsToFollow[currentWaypointIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypointIndex++;
+            if (currentWaypointIndex >= waypointsToFollow.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypointsToFollow[currentWaypointIndex].transform.position, Time.deltaTime * waypointFollowSpeed);
+    }
+
+    public override void Activate()
+    {
+        state = BeefHostileState.WaitingStatic;
+        timeCounter = 0f;
+        currentWaypointIndex = 0;
+    }
+
+    public override void Deactivate()
+    {
+        state = BeefHostileState.None;
+    }
+}
+
+public enum BeefHostileState
+{
+    None,
+    WaitingStatic,
+    Roaming,
+}
